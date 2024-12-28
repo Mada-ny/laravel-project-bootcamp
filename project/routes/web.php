9782a83ebf54d4ad32c8e04be8bd5d9f08b90f5a
Madany Doumbia <madany.doumbia@epitech.eu>
2024-12-28 12:59:59 +0000

    end

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home')->name('home');
+Route::get("/", [\App\Http\Controllers\HomeController::class,"index"])->name("home");
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
+Route::get('posts/{post}', [PostController::class,'show'])->name('post.show');
